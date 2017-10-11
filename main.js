fc30858611d17d851111bb5b5e36634a70f6f0c7
jmrigali <jmrigali@ymail.com>
2017-10-11 09:34:13 -0600

    added challenges

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-
 function changePic (){
   var main = document.getElementById('image');
     main.src = 'images/fallow-deer-602253__480.jpg';
 }
+
+
+//Change the headings text color to blue
+//HINT: it's a class!
+
+
+//Change the button's background-color to green
+
+
+//Change the paragraph font to 'Tahona'
+
+
+//Change the entire documents background color to yellow
