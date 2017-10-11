16caa4f32df8586d223ce41624a864e103e236d3
jmrigali <jmrigali@ymail.com>
2017-10-11 09:44:24 -0600

    solutions to challenges

--- main.js
+++ main.js
@@ -6,12 +6,19 @@ function changePic (){
 
 //Change the headings text color to blue
 //HINT: it's a class!
-
+var headings = document.getElementsByClassName('headings');
+for(var i = 0; i < headings.length; i++){
+  headings[i].style.color = 'blue';
+}
 
 //Change the button's background-color to green
+var button = document.getElementById('button');
+button.style.backgroundColor = 'green';
 
-
-//Change the paragraph font to 'Tahona'
-
+//Change the paragraph font to 'Arial'
+var paragraph = document.getElementsByClassName('paragraph')[0];
+paragraph.style.fontFamily =  'Arial';
 
 //Change the entire documents background color to yellow
+var body = document.getElementsByTagName('body')[0];
+body.style.backgroundColor= 'yellow';
